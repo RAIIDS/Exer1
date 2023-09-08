4d4005147a3b8a064ad706676d037f7f0cfe5ab8
RAIIDS <133626563+RAIIDS@users.noreply.github.com>
2023-09-08 11:25:25 +0800

    Update javascript.js

--- java.js/javascript.js
+++ java.js/javascript.js
@@ -1,21 +1,19 @@
 function displayProduct() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
 
-  let total = firstNum * secondNum;
+  let product = firstNum * secondNum;
   document.getElementById("answer1").innerHTML = `The product of ${firstNum} and ${secondNum} is: ${product}`;
 }
 
- document.getElementById('productButton').addEventListener("click", displayProduct);
-
+document.getElementById('productButton').addEventListener("click", displayProduct);
 
 function displayDifference() {
-  let firstNum = Number(document.getElementById('firstNum').innerHTML)
-  let secondNum = Number(document.getElementById('secondNum').innerHTML)
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
 
-
-  let total = firstNum - secondNum;
- document.getElementById("answer2").innerHTML = `The difference between ${firstNum} and ${secondNum} is: ${difference}`;
+  let difference = firstNum - secondNum;
+  document.getElementById("answer2").innerHTML = `The difference between ${firstNum} and ${secondNum} is: ${difference}`;
 }
 
-  document.getElementById('differenceButton').addEventListener("click", displayDifference);
+document.getElementById('differenceButton').addEventListener("click", displayDifference);
